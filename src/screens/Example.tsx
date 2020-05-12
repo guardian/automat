87cdfd3d507f3e97b07f3131834149cba285be7a
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-05-12 15:58:10 +0100

    Adds React Helmet

--- src/screens/Example.tsx
+++ src/screens/Example.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Counter } from '../components/Counter';
 import { useHistory } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 export const Example = () => {
   const history = useHistory();
   return (
     <div>
+      <Helmet>
+        <title>Automat UI | Example</title>
+      </Helmet>
       <h2>Example</h2>
       <Counter />
       <p>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 export const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Automat UI | Home</title>
+      </Helmet>
       <h2>Home</h2>
       <ul>
         <li>
