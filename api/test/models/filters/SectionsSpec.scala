81b776b2478eb42ee29cd305c677a8e426b819b2
Nicolas Long <nicolas.long@theguardian.com>
2020-05-21 12:37:30 +0100

    Generate meta via Scalacheck generator

--- api/test/models/filters/SectionsSpec.scala
+++ api/test/models/filters/SectionsSpec.scala
@@ -4,21 +4,22 @@ import org.scalatestplus.play.PlaySpec
 import models.filters.Sections
 import models.Meta
 import models.TextList
+import models.FilterHelpers.sampleMeta
 
 class SectionsSpec extends PlaySpec {
 
   "pass when no sections specified" in {
-    val m = Meta("sport")
+    val m = sampleMeta()
     Sections.run(m, None) mustBe true
   }
 
   "pass when sections includes meta section" in {
-    val m = Meta("sport")
+    val m = sampleMeta(sectionId = Some("sport"))
     Sections.run(m, Some(TextList(List("culture", "sport")))) mustBe true
   }
 
   "fail when page section not in section whitelist" in {
-    val m = Meta("sport")
+    val m = sampleMeta(sectionId = Some("sport"))
     Sections.run(m, Some(TextList(List("culture", "news")))) mustBe false
   }
 }
