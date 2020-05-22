fefa140cecaf22270a95385c0e9c8195aa220ccf
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-05-22 19:19:01 +0100

    Adds visibility icons

--- ui/src/components/ListTests.tsx
+++ ui/src/components/ListTests.tsx
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
 
 type Variant = string;
 type Section = string;
@@ -46,24 +47,31 @@ const testInfoStyles = css`
   margin-top: 0;
 `;
 
+const testHeaderStyles = css`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 export const ListTests = ({ tests, slot }: Props): JSX.Element => (
   <Grid container className={cx(rootStyles)} spacing={2}>
     <Grid item xs={12}>
       {tests.map((test: Test) => (
         <Card className={cx(paperStyles)}>
-          <Typography component="p" variant="h6">
-            <Link to={`/slots/${slot.id}/tests/${test.id}`} className={testLinkStyles}>
-              {test.name}
-            </Link>
-          </Typography>
+          <div className={testHeaderStyles}>
+            <div>
+              <Typography component="p" variant="h6">
+                <Link to={`/slots/${slot.id}/tests/${test.id}`} className={testLinkStyles}>
+                  {test.name}
+                </Link>
+              </Typography>
+            </div>
+            {test.enabled ? <Visibility /> : <VisibilityOff />}
+          </div>
+
           <p className={testInfoStyles}>{test.description}</p>
           <p className={testInfoStyles}>{test.variants.length} variants</p>
-          <p className={testInfoStyles}>
-            Sections:{' '}
-            {test.sections.map((section) => (
-              <>{section}</>
-            ))}
-          </p>
         </Card>
       ))}
     </Grid>
