442e8070becf67db6f9ebbdd0e03b221c302598b
Tom Wey <tom.wey@theguardian.com>
2020-06-11 14:01:11 +0100

    Add author to tests
    In future this will be derived by the API from the auth token (or
    similar) but for now this gets us moving.

--- api/app/models/test.scala
+++ api/app/models/test.scala
@@ -2,12 +2,23 @@ package models
 
 import play.api.libs.json._
 
+case class Author(
+    id: String,
+    firstName: String,
+    lastName: String
+)
+
+object Author {
+  implicit val authorFmt = Json.format[Author]
+}
+
 case class Test(
     id: String,
     name: String,
     description: String,
     isEnabled: Boolean,
-    variants: List[String]
+    variants: List[String],
+    author: Author
 )
 
 object Test {
--- api/test/targeting_spec.scala
+++ api/test/targeting_spec.scala
@@ -5,19 +5,13 @@ class TargetingSpec extends PlaySpec {
   "findMatches" must {
     "return a list of matching tests" in {
       val targeting = new Targeting(section = "football")
-      var test1 = Test(
+      var test1 = buildTest(
         id = "test1",
-        name = "Test 1",
-        description = "example test",
-        isEnabled = true,
-        variants = List("foo", "bar")
+        name = "Test 1"
       )
-      val test2 = Test(
+      val test2 = buildTest(
         id = "test2",
-        name = "Test 2",
-        description = "example test",
-        isEnabled = true,
-        variants = List("foo", "bar")
+        name = "Test 2"
       )
 
       val matches = Targeting.findMatches(targeting, List(test1, test2))
@@ -26,19 +20,13 @@ class TargetingSpec extends PlaySpec {
     }
     "return an empty list when there are no matches" in {
       val targeting = new Targeting(section = "cycling")
-      var test1 = Test(
+      var test1 = buildTest(
         id = "test1",
-        name = "Test 1",
-        description = "example test",
-        isEnabled = true,
-        variants = List("foo", "bar")
+        name = "Test 1"
       )
-      val test2 = Test(
+      val test2 = buildTest(
         id = "test2",
-        name = "Test 2",
-        description = "example test",
-        isEnabled = true,
-        variants = List("foo", "bar")
+        name = "Test 2"
       )
 
       val matches = Targeting.findMatches(targeting, List(test1, test2))
@@ -46,4 +34,25 @@ class TargetingSpec extends PlaySpec {
       matches mustBe List(test1, test2)
     }
   }
+
+  def buildTest(
+      id: String = "test",
+      name: String = "Test",
+      description: String = "Example test",
+      isEnabled: Boolean = true,
+      variants: List[String] = List("foo", "bar"),
+      author: Author = Author(
+        id = "example.user@guardian.co.uk",
+        firstName = "Example",
+        lastName = "User"
+      )
+  ): Test =
+    Test(
+      id = id,
+      name = name,
+      description = description,
+      isEnabled = isEnabled,
+      variants = variants,
+      author = author
+    )
 }
